f7de5d109656140565eec3c125b8650900590395
Mitzy <rios_mit@yahoo.com>
2023-10-17 19:09:54 -0700

    First Comm

--- public/index.js
+++ public/index.js
@@ -1,9 +1,13 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    let response = await fetch('http://localhost:9001')
+    let resObj = await response.json()
+    console.log(resObj)
+
+    let countValue = resObj.value;
 
     function increment(){
         countValue++;
